bcfeef3046a89eff7e894acdf9e62e62f6d33068
Shanthi SA <shanthi.shanmugam@gmail.com>
2021-10-15 11:37:35 -0300

    Removed main in package.json as it was throwing a build error. Commented creating mkdir

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -e
 # installs node_modules based on what is in package-lock.json
 npm install
 npx parcel build index.html
-mkdir -p dist/mp3s
-cp mp3s/* dist/mp3s
-mkdir -p dist/audio
-cp audio/* dist/audio
-mkdir -p dist/images
-cp images/* dist/images
+# mkdir -p dist/mp3s
+# cp mp3s/* dist/mp3s
+# mkdir -p dist/audio
+# cp audio/* dist/audio
+# mkdir -p dist/images
+# cp images/* dist/images
 # while creating the file first time, run in command prompt chmod +x build.sh to give executable rights
